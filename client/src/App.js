a3f338ff6f50389f5ba37bd60cfc1c8b0da855b1
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-04-15 12:50:13 -0500

    bootstrap/reactstrap hello world

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {Route} from 'react-router-dom';
 import Home from './components/Home';
+import { Alert } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Hello World from React</h1>
+          <h1>Hello World From React</h1>
           <h2>{this.props.test}</h2>
+          <Alert color="primary">Hello World From ReactStrap</Alert>
           <Route path="/" component={Home} />
         </header>
       </div>
